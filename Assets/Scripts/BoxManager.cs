a82da330e7c032b5c6782cc7404981d8afd54a86
Eveciana21 <ericanddaisy@Erics-iMac.mynextlight.net>
2024-02-13 18:55:55 -0700

    finished project other than needing to add the actual objects

--- Assets/Scripts/BoxManager.cs
+++ Assets/Scripts/BoxManager.cs
@@ -1,58 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BoxManager : MonoBehaviour
 {
-    private string _correctGemOrder = "BlueRedGreen";
-    private string _enteredGemOrder = "";
+	private string _correctGemOrder = "BlueRedGreen";
+	private string _enteredGemOrder = "";
 
-    [SerializeField] private Animator boxAnimator;
+	[SerializeField] private Animator boxAnimator;
+	
+	
 
-    private int _amountOfGems = 3;
-    private int _currentGem = 0;
+	private int _amountOfGems = 3;
+	private int _currentGem = 0;
 
-    [SerializeField] private Gem[] _gemsInScene;
+	[SerializeField] private Gem[] _gemsInScene;
+
+	[SerializeField] private UnityEvent _gameIsWon;
 
 
-    public void GemSelect(Gem currentSelectedGem)
-    {
-        //add the color of the gem to the enteredGemOrder
-        _enteredGemOrder += currentSelectedGem.gemColor;
 
-        //increment our currentGem
-        _currentGem++;
+	public void GemSelect(Gem currentSelectedGem)
+	{
+		//add the color of the gem to the enteredGemOrder
+		_enteredGemOrder += currentSelectedGem.gemColor;
 
-        currentSelectedGem.ChangeEmission(true);
+		//increment our currentGem
+		_currentGem++;
 
-        //if currentGem == amountOfGems, compare enteredOrder to correctOrder
-        if (_currentGem == _amountOfGems)
-        {
-            CompareGemOrder();
-        }
-    }
+		currentSelectedGem.ChangeEmission(true);
 
-    void CompareGemOrder()
-    {
-        if (_enteredGemOrder == _correctGemOrder)
-        {
-            OpenBox();
-        }
-        else
-        {
-            ResetGame();
-        }
-    }
-    void OpenBox()
-    {
-        boxAnimator.SetTrigger("Open");
-    }
+		//if currentGem == amountOfGems, compare enteredOrder to correctOrder
+		if (_currentGem == _amountOfGems)
+		{
+			CompareGemOrder();
+		}
+	}
 
-    void ResetGame()
-    {
-        _currentGem = 0;
-        _enteredGemOrder = "";
-    }
+	void CompareGemOrder()
+	{
+		if (_enteredGemOrder == _correctGemOrder)
+		{
+			CompleteGame();
+			
+				
+		}
+		else
+		{
+			ResetGame();
+		}
+	}
+	void CompleteGame()
+	{
+		_gameIsWon.Invoke();
+	}
+
+	void ResetGame()
+	{
+		_currentGem = 0;
+		_enteredGemOrder = "";
+		
+		foreach(var Gem in _gemsInScene)
+		{
+			Gem.ChangeEmission(false);
+		}
+	}
 
 }
 
